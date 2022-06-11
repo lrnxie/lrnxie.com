0d06413da51e8a6297b8d4804afeaf850507971d
Lauren Xie <lrnxie@gmail.com>
2022-06-11 15:43:09 -0400

    Clean up

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,13 @@ export default function Home() {
         <title>Lauren Xie</title>
       </Head>
 
-      <div className="">
+      <div>
         <div className="mb-6">
-          <h1 className="text-4xl font-light tracking-wide mb-3">
+          <h1 className="text-4xl font-light tracking-wide mb-4">
             Hi, I'm{" "}
             <span className="font-bold tracking-normal">Lauren Xie</span>
           </h1>
-          <h2 className="text-gray-700 mb-7">
+          <h2 className="text-lg text-gray-700 mb-7">
             <span className="font-semibold">Web Developer</span> based in
             Toronto, Canada
           </h2>
@@ -30,7 +30,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="">
+        <div>
           <a
             className="text-gray-400 hover:text-gray-600"
             href="https://github.com/lrnxie"
