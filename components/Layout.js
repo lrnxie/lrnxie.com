49275ce4d3ef50f1457a786ee07cc200e2f5c4ea
Lauren Xie <lrnxie@gmail.com>
2022-06-11 21:30:33 -0400

    Add dark mode

---
+++ components/Layout.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+
+export default function Container({ children }) {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <div className="w-full h-screen max-w-2xl mx-auto py-5">
+      <Head>
+        <title>Lauren Xie</title>
+      </Head>
+
+      <nav className="flex justify-end pt-5 pb-10">
+        <button
+          className="w-9 h-9 rounded-lg bg-gray-700 dark:bg-slate-200"
+          onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+        >
+          {resolvedTheme === "dark" ? "☀️" : "🌙"}
+        </button>
+      </nav>
+
+      <main className="flex flex-col justify-center">{children}</main>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
-import Head from "next/head";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen bg-gray-50 text-gray-800 px-10 mx-auto">
-      <Head>
-        <title>Lauren Xie</title>
-      </Head>
-
+    <Layout>
       <div>
         <div className="mb-6">
           <h1 className="text-4xl font-light tracking-wide mb-4">
@@ -14,12 +10,12 @@ export default function Home() {
             <span className="font-bold tracking-normal">Lauren Xie</span>
           </h1>
 
-          <h2 className="text-lg text-gray-700 mb-7">
+          <h2 className="text-lg text-gray-700 dark:text-slate-200 mb-7">
             <span className="font-semibold">Web Developer</span> based in
             Toronto, Canada
           </h2>
 
-          <div className="text-gray-600">
+          <div className="text-gray-600 dark:text-slate-300">
             <p className="font-medium mb-2">🛠 Site is under construction 🛠</p>
             <p>
               In the meantime, feel free to reach out to me via the contacts
@@ -28,9 +24,9 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="text-gray-500 tracking-wide">
+        <div className="text-gray-500 tracking-wide dark:text-slate-400">
           <a
-            className="mr-4 hover:text-gray-600"
+            className="mr-4 hover:text-gray-700 dark:hover:text-slate-300"
             href="https://github.com/lrnxie"
             target="_blank"
             rel="noreferrer"
@@ -38,18 +34,21 @@ export default function Home() {
             GitHub
           </a>
           <a
-            className="mr-4 hover:text-gray-600"
+            className="mr-4 hover:text-gray-700 dark:hover:text-slate-300"
             href="https://twitter.com/lrnxie"
             target="_blank"
             rel="noreferrer"
           >
             Twitter
           </a>
-          <a className="hover:text-gray-600" href="mailto:lrnxie@gmail.com">
+          <a
+            className="hover:text-gray-700 dark:hover:text-slate-300"
+            href="mailto:lrnxie@gmail.com"
+          >
             Email
           </a>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
