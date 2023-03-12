d8d6b9dd1cf79dcf6fffcb7d1ec97317b0afd0a8
Lauren Xie <lrnxie@gmail.com>
2023-03-12 18:49:42 -0400

    Handle Spotify 204 status code

--- pages/api/spotify-playing.js
+++ pages/api/spotify-playing.js
@@ -2,6 +2,11 @@ import { getNowPlaying } from "../../lib/spotify";
 
 export default async function handler(req, res) {
   const response = await getNowPlaying();
+
+  if (response.status === 204 || response.status > 400) {
+    return res.status(200).json({ isPlaying: false });
+  }
+
   const data = await response.json();
 
   const isPlaying = data.is_playing;
