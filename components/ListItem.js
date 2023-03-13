f0e31d31cc1b4e099613847b0514b29324d75a05
Lauren Xie <lrnxie@gmail.com>
2023-03-13 15:48:58 -0400

    Fix list item title text overflow and hover effect

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,22 +1,24 @@
 export default function ListItem({ icon, link, title, newTab = true }) {
   return (
     <div className="flex items-center">
-      <div className="w-7 h-7 mr-3 fill-neutral-800 dark:fill-neutral-100">
+      <div className="flex-none w-7 h-7 mr-3 fill-neutral-800 dark:fill-neutral-100">
         {icon}
       </div>
 
-      {link ? (
-        <a
-          className="font-semibold border-b-2 border-indigo-500 hover:border-indigo-200 hover:dark:border-indigo-700 transition-colors ease-in-out duration-200"
-          href={link}
-          target={newTab ? "_blank" : "_self"}
-          rel="noopener noreferrer"
-        >
-          {title}
-        </a>
-      ) : (
-        <p>{title}</p>
-      )}
+      <div className="flex-1 truncate">
+        {link ? (
+          <a
+            className="font-semibold border-b border-indigo-500 hover:border-b-2 transition-all ease-in-out duration-100"
+            href={link}
+            target={newTab ? "_blank" : "_self"}
+            rel="noopener noreferrer"
+          >
+            {title}
+          </a>
+        ) : (
+          <p>{title}</p>
+        )}
+      </div>
     </div>
   );
 }
