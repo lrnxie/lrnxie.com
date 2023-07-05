82fc13d96a0bf023f333a75e951b1b79c179a147
Lauren Xie <lrnxie@gmail.com>
2023-07-05 12:49:16 -0400

    Switch to Next app router

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import "./globals.css";
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { Providers } from "./providers";
+
+export const metadata: Metadata = {
+  title: "Lauren Xie",
+  description: "",
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={`${inter.variable} font-sans`}
+      suppressHydrationWarning
+    >
+      <body className="bg-neutral-50 text-neutral-900 transition-colors duration-200 dark:bg-[#0C0D0F] dark:text-neutral-50">
+        <Providers>{children}</Providers>
+        <Analytics />
+      </body>
+    </html>
+  );
+}
